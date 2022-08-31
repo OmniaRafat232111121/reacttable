d34cb5b1f993735eb26d165ac58a24457aca132a
omnia rafat <76200523+OmniaRafat232111121@users.noreply.github.com>
2022-08-31 03:32:44 +0200

    lists

--- .history/src/App_20220831023240.jsx
+++ .history/src/App_20220831023240.jsx
@@ -3,7 +3,6 @@ import './App.css'
 //import { FilteringTable } from './components/FiltertingTable'
 //import { FilteringTable } from './components/FiltertingTable'
 import { Paginationtable } from './components/Paginationtable'
-import { RowSelection } from './components/RowSelection'
 //import { BasicTable } from './components/BasicTable'
 //import { SortingTable } from './components/sortingTable'
 
@@ -11,7 +10,7 @@ import { RowSelection } from './components/RowSelection'
 function App() {
   return (
     <div className="App">
-<RowSelection/>
+
     </div>
   )
 }
---
+++ .history/src/components/Checkbox_20220831031711.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Checkbox = () => {
+  return (
+    <div>
+    </div>
+  )
+}
+
+export default Checkbox
